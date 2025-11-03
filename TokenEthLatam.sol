036f793c85058344bc3e21c078b1a8f8630e4de2
Solange Gueiros <solangegueiros@gmail.com>
2025-11-03 10:19:04 -0500

    token EthLatam

--- TokenEthLatam.sol
+++ TokenEthLatam.sol
@@ -4,10 +4,10 @@ pragma solidity 0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract Token is ERC20, AccessControl {
+contract TokenEthLatam is ERC20, AccessControl {
 	bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-	constructor() ERC20("Stable Coin", "STC") {
+	constructor() ERC20("Token EthLatam Sao Paulo", "ELSP") {
     	_grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     	_grantRole(MINTER_ROLE, msg.sender);
 	}
